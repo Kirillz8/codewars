2a8cca5f1fe62a5e233f1928c00e7bc61a16a3bd
kirillz8 <akanin95@yandex.ru>
2024-10-11 14:44:35 +0300

    new 7kyu kata complete

---
+++ 7kyu/odder-than-the-rest.js
@@ -0,0 +1,18 @@
+// Create a method that takes an array/list as an input, and outputs the index at which the sole odd number is located.
+//
+//     This method should work with arrays with negative numbers. If there are no odd numbers in the array, then the method should output -1.
+//
+// Examples:
+//
+//     oddOne([2,4,6,7,10]) // => 3
+// oddOne([2,16,98,10,13,78]) // => 4
+// oddOne([4,-8,98,-12,-7,90,100]) // => 4
+// oddOne([2,4,6,8]) // => -1
+
+//SOLUTIONS
+const oddOne = (arr) => arr.findIndex(num => num % 2 !== 0 )
+
+//TESTS
+console.log(oddOne([2,4,6,7,10]))
+console.log(oddOne([2,16,98,10,13,78]))
+console.log(oddOne([2,4,6,8]))
